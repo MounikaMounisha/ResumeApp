bbbe28329b16fb64ced63a8a91d3245e23cab736
MounishaMounika <tulasimounisha123@gmail.com>
2020-06-12 21:05:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/resumeapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.resumeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "This is resume phase", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+}
